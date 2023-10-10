25bf89ff0bbe1a3012de87d67652909900f7b821
caueoliveiraaa <leo.siscobra@gmail.com>
2023-10-10 19:24:31 -0300

    Display data with JS

--- 02Output&&Syntax/jsOutput.js
+++ 02Output&&Syntax/jsOutput.js
@@ -9,5 +9,35 @@
 // The id attribute defines the HTML element. The innerHTML property defines the HTML content.
 
 function showResult() {
-    document.getElementById("emptyTag").innerHTML = 5 + 6
+    document.getElementById("emptyTag").innerHTML = `The result of 5 + 6 is ${5 + 6}`
 }
+
+function showHTML() {
+    // Using document.write() after an HTML document is loaded,
+    // will delete all existing HTML, therefore it should only
+    // be used for testing.
+    document.write(`The result of 5 + 6 is ${5 + 6}`)
+}
+
+function showAlert() {
+    // In JavaScript the window object is the global scope object.
+    // This means that variables, properties, and methods by default
+    // belong to the window object. This also means that specifying
+    // the window keyword is optional.
+    // Example: alert(`The result of 5 + 6 is ${5 + 6}`)
+    window.alert(`The result of 5 + 6 is ${5 + 6}`)
+}
+
+function showConsole() {
+    // The result will be printed on the console of the browser
+    console.log(`The result of 5 + 6 is ${5 + 6}`)
+}
+
+function printerPrint() {
+    // JavaScript does not have any print object or print methods.
+    // You cannot access output devices from JavaScript.
+    // The only exception is that you can call the window.print()
+    // method in the browser to print the content of the current window.
+    window.print()
+}
+
